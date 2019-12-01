b3f63c360b2e9f4baba7901cf68ff693dfaa248a
Андрей <upndrey@yandex.ru>
2019-12-01 15:49:08 +0700

    Добавил хэширование паролей

--- php/login.php
+++ php/login.php
@@ -4,9 +4,9 @@ $login = $_POST['login'];
 $login = mysqli_real_escape_string($connection, $login);
 $pass = $_POST['pass'];
 $pass = mysqli_real_escape_string($connection, $pass);
-$result = mysqli_query($connection, "(SELECT id FROM users WHERE login='$login' AND password='$pass')");
-$rows = mysqli_fetch_array($result);
-if($rows){
+$result = mysqli_query($connection, "(SELECT password FROM users WHERE login='$login')");
+$hash = mysqli_fetch_array($result);
+if(password_verify($pass, $hash[0])){
     session_start();
     $_SESSION['login'] = $login;
     if($login == "admin")
--- php/signup.php
+++ php/signup.php
@@ -8,6 +8,7 @@ $result =  mysqli_fetch_array($result);
 if(!$result){
     $pass = $_POST['pass'];
     $pass = mysqli_real_escape_string($connection, $pass);
+    $hash = password_hash($pass, PASSWORD_DEFAULT);
 
     $group = $_POST['group'];
     $group = mysqli_real_escape_string($connection, $group);
@@ -18,11 +19,15 @@ if(!$result){
     $lastName = $_POST['lastName'];
     $lastName = mysqli_real_escape_string($connection, $lastName);
 
-    $query = "INSERT INTO users (login, password, group_name, firstName, lastName) VALUES ('$login', '$pass', '$group', '$firstName', '$lastName')";
+    $query = "INSERT INTO users (login, password, group_name, firstName, lastName) VALUES ('$login', '$hash', '$group', '$firstName', '$lastName')";
     mysqli_query($connection, $query);
     session_start();
     $_SESSION['login'] = $login;
-    header('Location: ' . $_SERVER['HTTP_REFERER'] . 'pages/tests.php');
+    if($login == "admin")
+        header('Location: /pages/admin.php');
+    else
+        header('Location: /pages/tests.php');
+    exit;
 }
 else
     header('Location: ' . $_SERVER['HTTP_REFERER']);
